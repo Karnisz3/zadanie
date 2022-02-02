d9899e318c652dfbd4dd53784fa3fcfb43e943da
Jakub Wołosowicz <kubekwolos@gmail.com>
2022-02-02 11:24:45 +0100

    Database in container

--- app/container.php
+++ app/container.php
@@ -4,21 +4,13 @@ declare(strict_types = 1);
 use DI\ContainerBuilder;
 
 return (new ContainerBuilder())->addDefinitions([
-    "database" => function () {
+    PDO::class => function () {
         $dsn = sprintf("%s:host=%s;dbname=%s", $_ENV['DB_ENGINE'], $_ENV['DB_HOST'], $_ENV['DB_NAME']);
         $params = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ];
 
-        try {
-            $pdo = new PDO($dsn, $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $params);
-        } catch (PDOException $pdoEx) {
-            echo "Database problem ocurred\n";
-            echo $pdoEx->getMessage();
-            die();
-        }
-
-        return $pdo;
+        return new PDO($dsn, $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $params);
     }
 ])->build();
